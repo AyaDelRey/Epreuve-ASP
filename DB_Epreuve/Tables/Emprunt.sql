f6d0d1ae4dc84e3a678af7afe4ccacd0a749f73b
Marwa Chanu <m.chanu@interface3.be>
2025-03-11 10:34:40 +0100

    creation des tables principales

---
+++ DB_Epreuve/Tables/Emprunt.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.Emprunt (
+    [Emprunt_Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+    [Jeu_Id] UNIQUEIDENTIFIER NOT NULL,                                  
+    [Emprunteur_Id] UNIQUEIDENTIFIER NOT NULL,                          
+    [DateEmprunt] DATE NOT NULL,                          
+    [DateRetour] DATE NULL,                                
+    [EvaluationPreteur] DECIMAL(2, 1) CHECK ([EvaluationPreteur] BETWEEN 0 AND 5),
+    [EvaluationEmprunteur] DECIMAL(2, 1) CHECK ([EvaluationEmprunteur] BETWEEN 0 AND 5),
+    CONSTRAINT FK_Emprunt_Jeu FOREIGN KEY ([Jeu_Id]) REFERENCES dbo.Jeux([Jeu_Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_Emprunt_Utilisateur FOREIGN KEY ([Emprunteur_Id]) REFERENCES dbo.Utilisateur([Utilisateur_Id]) ON DELETE CASCADE
+)
---
+++ DB_Epreuve/Tables/Jeux.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Jeux]
+(
+	[Jeu_Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+	[Nom] VARCHAR(255) NOT NULL,                
+    [Description] TEXT,                         
+    [AgeMin] INT CHECK ([AgeMin] >= 0),         -- Âge minimum recommandé (doit être >= 0)
+    [AgeMax] INT CHECK ([AgeMax] >= [AgeMin]),  -- Âge maximum recommandé (doit être >= AgeMin)
+    [NbJoueurMin] INT CHECK ([NbJoueurMin] > 0),-- Nombre minimum de joueurs (doit être > 0)
+    [NbJoueurMax] INT CHECK ([NbJoueurMax] >= [NbJoueurMin]), -- Nombre maximum de joueurs (>= NbJoueurMin)
+    [DureeMinute] DECIMAL(3, 1) CHECK ([DureeMinute] >= 0),   -- Durée approximative en minutes
+    [DateCreation] DATE DEFAULT GETDATE()       -- Date de création de l'enregistrement
+)
