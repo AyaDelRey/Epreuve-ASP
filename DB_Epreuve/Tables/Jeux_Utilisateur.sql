73ff0f24e267780b7ed74a375d155bc506d5f796
Marwa Chanu <m.chanu@interface3.be>
2025-03-11 15:09:17 +0100

    on delete no action et publish

--- DB_Epreuve/Tables/Jeux_Utilisateur.sql
+++ DB_Epreuve/Tables/Jeux_Utilisateur.sql
@@ -5,7 +5,7 @@
     [Jeu_Id] UNIQUEIDENTIFIER NOT NULL,                -- Référence au GUID du jeu
     [DateAcquisition] DATE DEFAULT GETDATE(),-- Optionnel : date d'acquisition
     [Etat] VARCHAR(20) NOT NULL,
-    CONSTRAINT FK_Utilisateur_Jeux_Utilisateur FOREIGN KEY ([Utilisateur_Id]) REFERENCES dbo.Utilisateur([Utilisateur_Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_Utilisateur_Jeux_Utilisateur FOREIGN KEY ([Utilisateur_Id]) REFERENCES dbo.Utilisateur([Utilisateur_Id]) ON DELETE NO ACTION,
     CONSTRAINT FK_Utilisateur_Jeux_Jeux FOREIGN KEY ([Jeu_Id]) REFERENCES dbo.Jeux([Jeu_Id]) ON DELETE CASCADE,
     CONSTRAINT UQ_Utilisateur_Jeux UNIQUE ([Utilisateur_Id], [Jeu_Id]),
     CONSTRAINT CK_Etat_Jeu CHECK ([Etat] IN ('Neuf', 'Abimé', 'Incomplet')),
