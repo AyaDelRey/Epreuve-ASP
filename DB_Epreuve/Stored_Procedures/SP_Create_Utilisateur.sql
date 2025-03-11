ce8797a25ffcffaa63d52836fffb4a1d4681e682
Marwa Chanu <m.chanu@interface3.be>
2025-03-11 14:27:42 +0100

    ajout des Stored_Procedures

---
+++ DB_Epreuve/Stored_Procedures/SP_Create_Utilisateur.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[SP_Create_Utilisateur]
+    @Email NVARCHAR(320),
+    @Password VARBINARY(64),
+    @Salt UNIQUEIDENTIFIER,
+    @Pseudo NVARCHAR(64)
+AS
+BEGIN
+    INSERT INTO dbo.Utilisateur (Email, Password, Salt, Pseudo, CreatedAt)
+    VALUES (@Email, @Password, @Salt, @Pseudo, GETDATE());
+END;
