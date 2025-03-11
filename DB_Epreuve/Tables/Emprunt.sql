8534e8f8bb3fa10a98fcb98456b795e79eb3ed50
Marwa Chanu <m.chanu@interface3.be>
2025-03-11 16:02:47 +0100

    ajout gitignore au dossier

--- DB_Epreuve/Tables/Emprunt.sql
+++ DB_Epreuve/Tables/Emprunt.sql
@@ -7,6 +7,7 @@
     [EvaluationPreteur] DECIMAL(2, 1) CHECK ([EvaluationPreteur] BETWEEN 0 AND 5) NULL,
     [EvaluationEmprunteur] DECIMAL(2, 1) CHECK ([EvaluationEmprunteur] BETWEEN 0 AND 5) NULL,
     
+    CONSTRAINT PK_Emprunt PRIMARY KEY ([Emprunt_Id]),
     CONSTRAINT FK_Emprunt_Jeux_Utilisateur FOREIGN KEY ([Jeux_Utilisateur_Id]) REFERENCES dbo.Jeux_Utilisateur([Jeux_Utilisateur_Id]) ON DELETE CASCADE,
     CONSTRAINT FK_Emprunt_Utilisateur FOREIGN KEY ([Emprunteur_Id]) REFERENCES dbo.Utilisateur([Utilisateur_Id]) ON DELETE CASCADE
 );
