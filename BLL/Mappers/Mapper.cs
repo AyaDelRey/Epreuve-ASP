97ac26392aecfd3ea40f7896081c6014dd7b5ed8
Marwa Chanu <m.chanu@interface3.be>
2025-03-12 12:15:48 +0100

    BLL jeu

---
+++ BLL/Mappers/Mapper.cs
@@ -0,0 +1,47 @@
+﻿using BLL.Entities;
+using D = DAL.Entities;
+
+namespace BLL.Mappers
+{
+    internal static class Mapper
+    {
+        #region Jeux
+        // Conversion de l'entité DAL.Jeu vers BLL.Jeu
+        public static Jeu ToBLL(this D.Jeu jeu)
+        {
+            if (jeu is null) throw new ArgumentNullException(nameof(jeu));
+
+            return new Jeu(
+                jeu.Jeu_Id,                // Identifiant du jeu
+                jeu.Nom,                   // Nom du jeu
+                jeu.Description,           // Description du jeu
+                jeu.MinAge,                // Âge minimum recommandé
+                jeu.MaxAge,                // Âge maximum recommandé
+                jeu.MinPlayers,            // Nombre minimum de joueurs
+                jeu.MaxPlayers,            // Nombre maximum de joueurs
+                jeu.DurationMinutes,       // Durée d'une partie (en minutes)
+                DateOnly.FromDateTime(jeu.CreationDate)  // Conversion de DateTime en DateOnly
+            );
+        }
+
+        // Conversion de l'entité BLL.Jeu vers DAL.Jeu
+        public static D.Jeu ToDAL(this Jeu jeu)
+        {
+            if (jeu is null) throw new ArgumentNullException(nameof(jeu));
+
+            return new D.Jeu()
+            {
+                Jeu_Id = jeu.Jeu_Id,                  // Identifiant du jeu
+                Nom = jeu.Nom,                        // Nom du jeu
+                Description = jeu.Description,        // Description du jeu
+                MinAge = jeu.MinAge,                  // Âge minimum recommandé
+                MaxAge = jeu.MaxAge,                  // Âge maximum recommandé
+                MinPlayers = jeu.MinPlayers,          // Nombre minimum de joueurs
+                MaxPlayers = jeu.MaxPlayers,          // Nombre maximum de joueurs
+                DurationMinutes = jeu.DurationMinutes, // Durée d'une partie (en minutes)
+                CreationDate = jeu.CreationDate.ToDateTime(new TimeOnly()) // Conversion de DateOnly en DateTime
+            };
+        }
+        #endregion
+    }
+}
---
+++ BLL/Services/JeuService.cs
@@ -0,0 +1,70 @@
+﻿using BLL.Entities;
+using BLL.Mappers;
+using Common.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class JeuService : IJeuRepository<Jeu>
+    {
+        private IJeuRepository<DAL.Entities.Jeu> _jeuService;
+        private IUtilisateurRepository<DAL.Entities.Utilisateur> _userService;
+
+        public JeuService(
+            IJeuRepository<DAL.Entities.Jeu> jeuService,
+            IUtilisateurRepository<DAL.Entities.Utilisateur> userService
+            )
+        {
+            _jeuService = jeuService;
+            _userService = userService;
+        }
+
+        // Méthode pour supprimer un jeu
+        public void Delete(Guid jeu_id)
+        {
+            _jeuService.Delete(jeu_id);
+        }
+
+        // Récupérer tous les jeux
+        public IEnumerable<Jeu> Get()
+        {
+            // Mappage des jeux DAL vers BLL
+            return _jeuService.Get().Select(dal => dal.ToBLL());
+        }
+
+        // Récupérer un jeu par son identifiant
+        public Jeu Get(Guid jeu_id)
+        {
+            return _jeuService.Get(jeu_id).ToBLL();
+        }
+
+        // Récupérer les jeux d'un utilisateur spécifique
+        public IEnumerable<Jeu> GetFromUser(Guid utilisateur_id)
+        {
+            // Vérifie que la méthode GetFromUser existe dans ton repository DAL.
+            // Elle doit retourner les jeux associés à cet utilisateur.
+            var dalJeux = _jeuService.GetFromUser(utilisateur_id);
+
+            // Mappage des jeux DAL vers BLL
+            return dalJeux.Select(dal => dal.ToBLL());
+        }
+
+        // Insérer un nouveau jeu
+        public Guid Insert(Jeu jeu)
+        {
+            // Conversion de l'entité BLL en DAL avant l'insertion
+            return _jeuService.Insert(jeu.ToDAL());
+        }
+
+        // Mettre à jour un jeu existant
+        public void Update(Guid jeu_id, Jeu jeu)
+        {
+            // Conversion de l'entité BLL en DAL avant la mise à jour
+            _jeuService.Update(jeu_id, jeu.ToDAL());
+        }
+    }
+}
